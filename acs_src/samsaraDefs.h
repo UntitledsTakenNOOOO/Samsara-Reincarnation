6fe7d90a81e359616dd85d772ac01c22c4b86f29
Maggle <64440539+Magglus@users.noreply.github.com>
2020-12-16 12:14:01 -0800

    Some Assorted Changes
    - Added option for Blood Guns Akimbo to be an inventory item with a cooldown or to have original behavior
    - Further Smoothed SW Riot Gun firing animations
    - Duke's Explosive Shotgun and Lo Wang's Grenade Launcher now use shells (former uses 3)
    - Added complete hybrid pickup sprites for Eleena, IP Freeley, Caleb, Blake Stone, and Jack Curtis
    - Decreased Radius damage of Lo Wang's Grenade Launcher to make it more balanced (and to make it overall safer to use in cramped spaces)
    - Lo Wang no longer gets Rockets from shell pickups
    - Weapon switching for Jon's weapons is now much longer
    - Powerslave Machete now goes into its slower animation at a much further range
    - Fixed one of Powerslave Machete's swipes doing 18 damage instead of 25

--- acs_src/samsaraDefs.h
+++ acs_src/samsaraDefs.h
@@ -97,7 +97,7 @@
 #define SPEED_FORWARD       15
 #define SPEED_SIDE          13
 
-#define UNLOADCOUNT 71
+#define UNLOADCOUNT 72
 
 #define P_COUNT 2
 #define P_QUAD  0
@@ -342,7 +342,7 @@ int UnloadRemove[UNLOADCOUNT] =
 	"GibfistSide", "God Hand", "BStoneProtection", "IGotMyUnique", "DBSHOT", "DBSHOT1", "DB1", "DB2", "DB3", "DB4", "DB5", "DB6",
 	"DB7", "DB8", "DB9", "DB10", "DB11", "DB12", "DB13", "DB14", "DB15", "DB16", "DB17",
     "DB18", "DB19", "StopFromPickingShit", "EleenaTauntCooldown", "LeonardTauntCooldown", "CalebTauntCooldown", "LoWangTauntCooldown",
-	"JonTauntCooldown", "StopFromPickingTurrets_", "StrifeBeaconCooldown"
+	"JonTauntCooldown", "StopFromPickingTurrets_", "StrifeBeaconCooldown", "BloodGunsAkimboCooldown"
 };
 
 int PowerOutVols[5] = {96, 104, 112, 120, 127};
