bc31ab436dccf0e901dd10bfdf6d3e11ab68195b
TheSuperDave938 <71569097+superdave938@users.noreply.github.com>
2024-08-10 14:01:42 -0500

    pk3 building system
    repurposed the ipk3 building system used for Chalice of Glory for samsara, should make compiling dev builds easier (made by Nash Muhandes)

---
+++ build_pk3.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM file name of the built PK3, feel free to edit - Stolen from COG lol
+set pk3name="Samsara Reincarnation v2.1 Dev Build"
+
+
+
+REM !!! DO NOT EDIT ANYTHING BEYOND THIS POINT !!!!
+REM !!! DO NOT EDIT ANYTHING BEYOND THIS POINT !!!!
+REM !!! DO NOT EDIT ANYTHING BEYOND THIS POINT !!!!
+
+if exist %pk3name%.pk3 del %pk3name%.pk3
+7za.exe a -r -x!.git\ -x!.vscode\ -x!*.gitattributes -x!*.gitignore -x!src\ -x!psd\ -x!resources\ -xr!*.bat -xr!*.pk3 -xr!*.dbs -xr!*.exe -xr!*.dll -xr!*.bak -xr!*.backup* -xr!*.workspace -xr!*.psd -xr!*.blend -xr!*.lnk -xr!*.wl6 -xr!*.autosave* -xr!*.ps1* -ssw -tzip -mx9 .\%pk3name%.pk3 "*"
