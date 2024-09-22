375abed417e9ac778a0b346359abda58e432ac6a
cubebert-github <cubebertoffical@gmail.com>
2024-09-22 14:40:59 -0700

    Updated build_pk3

--- build_pk3.bat
+++ build_pk3.bat
@@ -10,4 +10,4 @@ REM !!! DO NOT EDIT ANYTHING BEYOND THIS POINT !!!!
 REM !!! DO NOT EDIT ANYTHING BEYOND THIS POINT !!!!
 
 if exist %pk3name%.pk3 del %pk3name%.pk3
-7za.exe a -r -x!.git\ -x!.vscode\ -x!*.gitattributes -x!*.gitignore -x!src\ -x!psd\ -x!resources\ -xr!*.bat -xr!*.pk3 -xr!*.dbs -xr!*.exe -xr!*.dll -xr!*.bak -xr!*.backup* -xr!*.workspace -xr!*.psd -xr!*.blend -xr!*.lnk -xr!*.wl6 -xr!*.autosave* -xr!*.ps1* -ssw -tzip -mx9 .\%pk3name%.pk3 "*"
+7za.exe a -r -x!.git\ -x!.github\ -x!.vscode\ -x!*.gitattributes -x!*.gitignore -x!src\ -x!psd\ -x!resources\ -xr!*.bat -xr!README.md -xr!*.pk3 -xr!*.dbs -xr!*.exe -xr!*.dll -xr!*.bak -xr!*.backup* -xr!*.workspace -xr!*.psd -xr!*.blend -xr!*.lnk -xr!*.wl6 -xr!*.autosave* -xr!*.ps1* -ssw -tzip -mx9 .\%pk3name%.pk3 "*"
